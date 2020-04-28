b9b4f2f0feb7f3a24da24fe83c03653ab8cf5131
LTurns <lizzieturney@gmail.com>
2020-04-28 18:00:46 +0100

    testing player1 and player2 names

--- Battle/app.rb
+++ Battle/app.rb
@@ -5,4 +5,14 @@ class Battle < Sinatra::Base
   get '/' do
     'Testing infrastructure working!'
   end
+
+  get '/form' do
+    erb(:index)
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
 end
--- Battle/spec/testing_infrastructure_spec.rb
+++ Battle/spec/testing_infrastructure_spec.rb
@@ -1,6 +1,18 @@
+require 'spec_helper'
+
 feature 'Testing infrastructure' do
   scenario 'Can run app and check page content' do
     visit('/')
     expect(page).to have_content 'Testing infrastructure working!'
+  end
+  scenario 'expect players to input name and see on screen' do
+    visit('/form')
+
+    fill_in :player_1_name, with: 'Hibo'
+    fill_in :player_2_name, with: 'Lizzie'
+
+    click_button "Submit"
+
+    expect(page).to have_content("Hibo vs. Lizzie")
   end
 end
